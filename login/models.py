5242cbd1c914145c4d91738b68cf3e6526a7e699
dangqhuy <dangqhuy2411@gmail.com>
2018-05-23 15:54:20 +0700

    fix syntax

--- login/models.py
+++ login/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
 	username = models.CharField(max_lenght = 50)
-	password = model.CharField(max_lenght = 50)
-
-
+	password = models.CharField(max_lenght = 50)
+	email = models.CharField(max_lenght = 100)
+	
+	
--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from login import views
 urlpatterns = [
-	path('', views.login, name = 'login')
+	path('', views.index, name = 'index')
 ]
